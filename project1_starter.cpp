792a627615e33334b7addfe9294951fd886ba371
jhytai <josue.tai@csu.fullerton.edu>
2023-10-01 20:24:28 -0700

    Created main() and Interval struct

--- project1_starter.cpp
+++ project1_starter.cpp
@@ -1,2 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
 using namespace std;
+
+struct  Interval
+{
+    int start;
+    int end;
+};
+
+
+
+int main()
+{
+    ifstream    fin;
+    ofstream    fout;
+    string      line;
+
+    fin.open("Input.txt");
+    if (fin.fail())
+        {
+        fout.open("Output.txt");
+        fout << "Error: Unable to open 'Input.txt' file for reading...\n";
+        fout.close();
+
+        cout << "\nError: Unable to open 'Input.txt' file for reading...\n";
+        cout << "Program ended.\n\n";
+
+        return 0;
+        }
+    else
+        {
+        fout.open("Output.txt");
+        while (getline(fin, line))
+            {
+            // algorithm code goes here
+            }
+        
+        fin.close();
+        fout.close();
+
+        return 0;
+        }
+}
