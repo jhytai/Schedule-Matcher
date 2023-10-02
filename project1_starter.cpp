1104a1e06a73cd12cd5b4a6e3ca4fac26e9a2423
jhytai <josue.tai@csu.fullerton.edu>
2023-10-01 22:33:15 -0700

    Finished implementing main()

--- project1_starter.cpp
+++ project1_starter.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 struct  Interval
@@ -50,7 +51,96 @@ int main()
         fout.open("Output.txt");
         while (getline(fin, line))
             {
-            // algorithm code goes here
+            stringstream        ss(line);
+            string              from;
+            string              to;
+            vector<Interval>    unavailable1;
+            vector<Interval>    unavailable2;
+            int                 workStart1;
+            int                 workStart2;
+            int                 workEnd1;
+            int                 workEnd2;
+            int                 meetingDuration;
+            vector<Interval>    combinedUnavailable;
+            vector<Interval>    mergedUnavailable;
+            vector<Interval>    combinedAvailable;
+            bool                first = false;
+
+            // Getting the 1st person's unavailable schedule
+            while (ss >> from >> to)
+                {
+                unavailable1.push_back({
+                    timeToMinutes(from.substr(2, from.size() - 3)),
+                    timeToMinutes(to.substr(1, to.size() - 3))
+                    });
+                }
+
+            // Getting the 1st person's working hours
+            getline(fin, line);
+            ss = stringstream(line);
+            ss >> from >> to;
+            workStart1 = timeToMinutes(from.substr(1, from.size() - 2));
+            workEnd1 = timeToMinutes(to.substr(1, to.size() - 2));
+
+            // Getting the 2nd person's unavailable schedule
+            getline(fin, line);
+            ss = stringstream(line);
+            while (ss >> from >> to)
+                {
+                unavailable2.push_back({
+                    timeToMinutes(from.substr(2, from.size() - 3)),
+                    timeToMinutes(to.substr(1, to.size() - 3))
+                    });
+                }
+
+            // Getting the 2nd person's working hours
+            getline(fin, line);
+            ss = stringstream(line);
+            ss >> from >> to;
+            workStart2 = timeToMinutes(from.substr(1, from.size() - 2));
+            workEnd2 = timeToMinutes(to.substr(1, to.size() - 2));
+
+            // Getting meeting duration
+            getline(fin, line);
+            ss = stringstream(line);
+            ss >> meetingDuration;
+
+            // Combining and merging unavailable schedules of both persons
+            combinedUnavailable = unavailable1;
+            combinedUnavailable.insert(combinedUnavailable.end(),
+                                       unavailable2.begin(),
+                                       unavailable2.end());
+            sort(combinedUnavailable.begin(), combinedUnavailable.end(),
+                 [](const Interval &a, const Interval &b) {
+                 return a.start < b.start; });
+            mergedUnavailable = mergeIntervals(combinedUnavailable);
+
+            // Getting combined schedule of availability for both persons
+            combinedAvailable = availableIntervals(mergedUnavailable,
+                                                   max(workStart1, workStart2),
+                                                   min(workEnd1, workEnd2));
+
+            fout << "[";
+            cout << "[";
+            first = true;
+            for (const auto &interval : combinedAvailable)
+                {
+                if (interval.end - interval.start >= meetingDuration)
+                    {
+                    if (!first)
+                        {
+                        fout << ", ";
+                        cout << ", ";
+                        }
+                    fout << "['" << minutesToTime(interval.start) << "', '"
+                         << minutesToTime(interval.end) << "']";
+                    cout << "['" << minutesToTime(interval.start) << "', '"
+                         << minutesToTime(interval.end) << "']";
+                    first = false;
+                    }
+                }
+            fout << "]" << endl;
+            cout << "]" << endl;
             }
 
         fin.close();
@@ -96,7 +186,7 @@ string  minutesToTime(int minutes)
 
 
 
-// ==== mergeIntervals =========================================================
+// ==== mergeIntervals ========================================================
 //
 // ============================================================================
 
@@ -138,7 +228,7 @@ vector<Interval>    mergeIntervals(const vector<Interval> &intervals)
 
 
 
-// ==== availableIntervals =========================================================
+// ==== availableIntervals ====================================================
 //
 // ============================================================================
 
